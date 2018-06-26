1b183b7529946441a8d53380af452a04679a7303
ceva94 <ceva94@hotmail.com>
2018-06-26 04:33:01 -0500

    mapa

--- js/aplica.js
+++ js/aplica.js
@@ -1,5 +1,32 @@
+function buscar(){
+	var texto = document.getElementById('texto').value;
+	if(texto.length > 0){
+
+		var deportes = $('.encontrar')
+
+		for(deporte of deportes){
+			if (deporte.textContent.toLowerCase().search(texto) != -1){
+				$(deporte).parent().attr('class','col-lg-3 col-md-4 col-sm-6 col-xs-12 mostrar')
+			} else{
+					$(deporte).parent().attr('class','ocultar')
+			}
+	
+		}
+
+	} else {
+		var deportes = $('.encontrar')
+		for(deporte of deportes){
+			$(deporte).parent().attr('class','col-lg-3 col-md-4 col-sm-6 col-xs-12 mostrar')
+		}
+
+	}
+
+}
+
+
 $(document).ready(function()
 {
+	$('#buscar').on('click',buscar)
 	$.ajax({
 		type: "GET",
 		url: "data/eventos.xml",
