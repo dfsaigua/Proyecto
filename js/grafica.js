3c31c810cd2f0a84964d7816a0840eeeee2de4bc
ceva94 <ceva94@hotmail.com>
2018-07-01 02:49:25 -0500

    grafico incluido

--- js/grafica.js
+++ js/grafica.js
@@ -13,7 +13,7 @@ $(document).ready(function()
 
 	var arc = d3.svg.arc()
     .outerRadius(radius - 10)
-    .innerRadius(radius - 100);
+    .innerRadius(0);
 	var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.population; });
