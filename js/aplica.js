ae759b6115df2e3836c7792b6376d6ee149d7df8
ceva94 <ceva94@hotmail.com>
2018-07-01 15:12:29 -0500

    cambios

--- js/aplica.js
+++ js/aplica.js
@@ -33,10 +33,10 @@ $(document).ready(function()
 	    success: function(xml){
 
 	    	$(xml).find('evento').each(function(){
-	          var sTitulo = $(this).find('titulo').text();
-	          var sTexto = $(this).find('texto').text();
-	          $("<h3></h3>").html(sTitulo).appendTo("#inf-event");
-	          $("<p></p>").html(sTexto).appendTo("#inf-event");
+	    		var sTitulo = $(this).find('titulo').text();
+	          	var sTexto = $(this).find('texto').text();
+	          	$("<h3></h3>").html(sTitulo).appendTo("#inf-event");
+	          	$("<p></p>").html(sTexto).appendTo("#inf-event");
 
 	        })
 	    }
